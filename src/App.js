7f6b2d647dc79aab9ceba594030455a094da9880
JeloRisk <mamuadjeromemarlo@gmail.com>
2021-08-28 16:02:29 +0800

    asas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 //
-import React from "react"
-import Card from "./Card.js"
+import React from 'react';
+import Card from './Card.js';
 
-import "./style.css"
+import './style.css';
 
-const App = () =>
-{
-    return(
-        <div className="app-main">
-            <Card name="Jerome" age={ 18} course = "Computer Science" />
-          <Card name="Alvin" age={ 19 } course = "Criminology" />
-            <Card name="Sandee" age={19} course="Bachelor of Special Needs Education" />
-            <Card name="Budoy" age={ 20} course = "Information Technology" />
-    </div>
-    )
-    
-}
+const App = () => {
+	return (
+		<div className='app-main'>
+			<Card name='Jerome' age={18} course='Computer Science' />
+			<Card name='Alvin' age={19} course='Criminolosgy' />
+			<Card name='Sandee' age={19} course='Bachelor of Special Needs Education' />
+			<Card name='Budoy' age={20} course='Information Technology' />
+		</div>
+	);
+};
 export default App;
